204afd5f2889573ed71245a6dfdb993e991b5fe2
ricodroid404 <ricodroid2024@gmail.com>
2024-02-17 17:17:18 +0900

    xmlのレイアウト作成

--- app/src/main/java/com/example/recyclerviewticktock/RecyclerAdapter.kt
+++ app/src/main/java/com/example/recyclerviewticktock/RecyclerAdapter.kt
@@ -3,17 +3,14 @@ package com.example.recyclerviewticktock
 import android.content.Context
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
-import com.google.common.reflect.Reflection.getPackageName
 
 class RecyclerAdapter(private val context: Context): RecyclerView.Adapter<ViewHolderItem>()  {
     private val videoList = listOf(
-        "android.resource://" + context.packageName + "/" + R.raw.bigbuckbunny,
         "android.resource://" + context.packageName + "/" + R.raw.move1,
-        "android.resource://" + context.packageName + "/" + R.raw.bigbuckbunny,
-        "android.resource://" + context.packageName + "/" + R.raw.move1,
-        "android.resource://" + context.packageName + "/" + R.raw.bigbuckbunny,
-        "android.resource://" + context.packageName + "/" + R.raw.move1,
-        // 他のビデオパスを追加
+        "android.resource://" + context.packageName + "/" + R.raw.move2,
+        "android.resource://" + context.packageName + "/" + R.raw.move3,
+        "android.resource://" + context.packageName + "/" + R.raw.move4,
+        "android.resource://" + context.packageName + "/" + R.raw.move5,
     )
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolderItem {
